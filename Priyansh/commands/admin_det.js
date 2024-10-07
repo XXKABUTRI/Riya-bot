9322fae7c305885734158b803a6b94d6487d42e2
XXKABUTRI <vanshikabatwal@gmail.com>
2024-10-07 20:30:03 +0530

    Update admin_det.js

--- Priyansh/commands/admin_det.js
+++ Priyansh/commands/admin_det.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "admin2",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩", 
+  credits: "Riya Roy", 
   description: "hihihihi",
   commandCategory: "no prefix",
   usages: "admin",
@@ -14,12 +14,12 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   if (event.body.indexOf("ADMIN")==0 || event.body.indexOf("Admin")==0 || event.body.indexOf("/Admin")==0 || event.body.indexOf("#admin")==0) {
     var msg = {
-        body: "🫅 𝐎𝐖𝐍𝐄𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 🫅",
+        body: "👸 𝐎𝐖𝐍𝐄𝐑÷ 𝗥𝗶𝗬𝗮 𝗥𝗼𝗬 👸",
         attachment: 
 fs.createReadStream(__dirname + `/noprefix/profile.png`)
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🫅", event.messageID, (err) => {}, true)
+    api.setMessageReaction("👸", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
