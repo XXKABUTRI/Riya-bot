fdbea9da25dbbd12cc5d5e89d22260ac86706dd1
XXKABUTRI <vanshikabatwal@gmail.com>
2024-10-08 10:49:06 +0530

    Update 0_ravi_good morning.js

--- Priyansh/commands/0_ravi_good morning.js
+++ Priyansh/commands/0_ravi_good morning.js
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("morning") ||
 react.includes("MORNING")) {
 		var msg = {
-				body: `💖💖💖𝐕𝐄𝐑𝐘 𝐆𝐎𝐎𝐃 𝐌𝐎𝐑𝐍𝐈𝐍𝐆 𝐉𝐀𝐀𝐍😻`,attachment: fs.createReadStream(__dirname + `/ravi/morning.gif`)
+				body: `💖💖💖𝐕𝐄𝐑𝐘 𝐆𝐎𝐎𝐃 𝐌𝐎𝐑𝐍𝐈𝐍𝐆 𝐉𝐀𝐀𝐍😻`,attachment: fs.createReadStream(__dirname + `/wall/morning.webp`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("💖", event.messageID, (err) => {}, true)
