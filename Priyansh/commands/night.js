c238b5a7c923c4217eb5b334c6ada93cf85f94d9
XXKABUTRI <vanshikabatwal@gmail.com>
2024-10-08 11:22:23 +0530

    Update night.js

--- Priyansh/commands/night.js
+++ Priyansh/commands/night.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	if (event.body.indexOf("Good night")==0 || event.body.indexOf("good night")==0 || event.body.indexOf("Gud night")==0 || event.body.indexOf("Gud nini")==0) {
 		var msg = {
 				body: "Good night 🌉✨ Bye tc 💫🥀 Sweet dreams 😴",
-				attachment: fs.createReadStream(__dirname + `/cache/night.jpg`)
+				attachment: fs.createReadStream(__dirname + `/wall/night.webp`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😴", event.messageID, (err) => {}, true)
