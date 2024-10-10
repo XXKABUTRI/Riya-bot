dc0926647dc19f0b776fa7edb5a8abffc763204a
𝄟✮͢🦋⃟≛⃝ 𓆩ᴋᵃʙᵘᴛᵃʀⁱ𓆪 ༒𝄟✮⃝❤ <vanshikabatwal@gmail.com>
2024-10-10 16:46:41 +0530

    Update god.js

--- Priyansh/events/god.js
+++ Priyansh/events/god.js
@@ -42,7 +42,7 @@ module.exports.run = async function({ api, event, Threads }) {
 
     formReport = formReport
     .replace(/\{task}/g, task);
-  var god = "61562335250325";
+  var god = "61566407324934";
 
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
